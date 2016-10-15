48dd629016cf590debc86d37e14d53f825e6544b
Christopher Mühl <christopher@padarom.io>
2016-10-15 19:24:08 +0200

    Remove everything that doesn't belong to awards

--- sql/install/020-userHistoryType.sql
+++
@@ -1,9 +0,0 @@
--- Create the User History Type table
-CREATE TABLE wcf1_unkso_user_history_type (
-    historyTypeID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    displayName VARCHAR(255) NOT NULL,
-    packageID INT(10) NOT NULL,
-    displayTemplate MEDIUMTEXT,
-    identifier VARCHAR(255) NOT NULL UNIQUE,
-    classPath VARCHAR(255) NOT NULL
-);
